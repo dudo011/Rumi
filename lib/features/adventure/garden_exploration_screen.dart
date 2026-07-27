c0467ec7e0cb156aa2068753358d7e89798b75f3
Mr.kkim <dh.kkim0104@gmail.com>
2026-07-28 08:23:32 +0900

    chore: label episode one P0 to P5 preview

--- lib/features/adventure/garden_exploration_screen.dart
+++ lib/features/adventure/garden_exploration_screen.dart
@@ -46,8 +46,8 @@ class GardenExplorationScreen extends StatelessWidget {
                     value: _DevelopmentPreview.episodeOne,
                     child: ListTile(
                       key: Key('open-episode-one-escape-foundation'),
-                      leading: Icon(Icons.account_tree_rounded),
-                      title: Text('새 방탈출 P0~P4 체험'),
+                      leading: Icon(Icons.nights_stay_rounded),
+                      title: Text('새 방탈출 P0~P5 체험'),
                     ),
                   ),
                   PopupMenuItem(
