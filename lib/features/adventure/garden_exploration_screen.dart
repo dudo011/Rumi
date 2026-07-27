08e3bdf3ac3a252943c5eb375afc967c43d32994
Mr.kkim <dh.kkim0104@gmail.com>
2026-07-27 17:54:48 +0900

    chore: run CI for complete Flame episode one

--- lib/features/adventure/garden_exploration_screen.dart
+++ lib/features/adventure/garden_exploration_screen.dart
@@ -4,6 +4,7 @@ import 'flame/episode_1_flame_screen.dart';
 import 'garden_mystery_v2_screen.dart';
 
 /// 기존 모험은 유지하면서 Flame 1단계 전체 체험판을 비교할 수 있게 한다.
+/// 전체 상호작용 검증용 빌드 진입점이다.
 class GardenExplorationScreen extends StatelessWidget {
   const GardenExplorationScreen({required this.onContinue, super.key});
 
